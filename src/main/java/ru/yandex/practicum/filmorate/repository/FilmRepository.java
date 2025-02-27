e795b2ae29fefc7a8c4cfa2b9a75821aafb31f09
PiTri <burova.dv@yandex.ru>
2025-02-27 16:05:07 +0300

    Project 11

--- src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
@@ -5,8 +5,12 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.List;
 
 public interface FilmRepository {
+
     Film addFilm(Film film);
+
     Film getFilmById(int id);
+
     List<Film> getAllFilms();
+
     void deleteFilm(int id);
 }
