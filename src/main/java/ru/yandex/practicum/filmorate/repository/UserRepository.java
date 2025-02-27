574e86a36eb22de1217870f33ec78294dcaaaa1a
PiTri <burova.dv@yandex.ru>
2025-02-27 16:05:07 +0300

    Project 11

--- src/main/java/ru/yandex/practicum/filmorate/repository/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/UserRepository.java
@@ -5,9 +5,14 @@ import ru.yandex.practicum.filmorate.model.User;
 import java.util.List;
 
 public interface UserRepository {
+
     User addUser(User user);
+
     User getUserById(long id);
+
     List<User> getAllUsers();
+
     void deleteUser(long id);
+
     boolean existsById(long id); // Новый метод
 }
